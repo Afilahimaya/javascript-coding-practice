ccf121c186d6c9649261891a59eadbe85eb598ca
Afilahimaya <afilahimaya@gmail.com>
2024-04-23 10:35:41 +0200

    adding files

---
+++ max-min-array.js
@@ -0,0 +1,3 @@
+const arr =  [2,9,4,10,5];
+console.log(Math.max(...arr));//10
+console.log(Math.min(...arr));//2
---
+++ random.js
@@ -0,0 +1,11 @@
+//Javascript Program to Generate a Random Number
+var a = Math.floor(Math.random() * (10-1) + 1);
+var d = (Math.random() * (10-1) + 1);
+//Math.floor(Math.random() * (highestNumber - lowestNumber)) + lowestNumber
+//Math.floor(Math.random() * (max - min + 1)) + min - for range
+var f = Math.floor(Math.random() * (50-20+1) + 20);
+var b = Math.random();
+console.log(a);
+console.log(b);
+console.log(d);
+console.log(f);
